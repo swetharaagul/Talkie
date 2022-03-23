f71c6206204bd5a93617b0fc0f0589f3de091d80
swetharaagul <101871775+swetharaagul@users.noreply.github.com>
2022-03-23 14:31:07 +0530

    Create TTS_BP.h

---
+++ TTS_BP.h
@@ -0,0 +1,10 @@
+#ifndef TALKIE_VOCAB_US_CLOCK_H_
+#define TALKIE_VOCAB_US_CLOCK_H_
+
+#include <Arduino.h>
+
+extern const uint8_t spA_HELLO[]       PROGMEM;
+extern const uint8_t spA_WORLD[]      PROGMEM;
+extern const uint8_t spA_DEMO[]        PROGMEM;
+extern const uint8_t spA_PROGRAM[]      PROGMEM;
+#endif /* TALKIE_VOCAB_US_CLOCK_H_ */
